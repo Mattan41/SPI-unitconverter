5afc92e5fe9ea10ee3b3708f19e5bf8c02829e8a
Mattan41 <Mats.kruskopf.eriksson@iths.se>
2024-04-19 14:10:19 +0200

    remove unused import

--- consumer/src/main/java/org/example/consumer/Main.java
+++ consumer/src/main/java/org/example/consumer/Main.java
@@ -5,7 +5,6 @@ import org.example.service.Converter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ServiceLoader;
-import java.util.StringJoiner;
 
 import static org.example.consumer.inputReader.inputIntChoice;
 
@@ -13,14 +12,12 @@ public class Main {
     public static void main(String[] args) {
 
         ServiceLoader<Converter> loader = ServiceLoader.load(Converter.class);
-
         List<Converter> converters = getConverters(loader);
 
         System.out.println("Choose a converter:");
 
         String converterMenu = getConverterMenu(converters);
         int choice = inputIntChoice((converterMenu), converters);
-
         useConverter(choice, converters);
     }
 
@@ -49,10 +46,9 @@ public class Main {
         for (int i = 0; i < converters.size(); i++) {
             String className = converters.get(i).getClass().getName();
             String shortClassName = className.substring(className.lastIndexOf(".") + 1);
-            convertMenu.append(i+1).append(". ").append(shortClassName).append("\n");
+            convertMenu.append(i + 1).append(". ").append(shortClassName).append("\n");
         }
 
         return convertMenu.toString();
     }
-
 }
